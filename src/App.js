f711f270353c5f41096040f8e926daa02862fea2
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-08-12 13:26:31 +0700

    hw done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Здесь скоро будет Skypro.Music
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
