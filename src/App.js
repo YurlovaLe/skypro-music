8d5c058135af0fcc1e4a7ec0056e43dbeb87740c
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-08-13 22:42:04 +0700

    add Sidebar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MenuList } from './components/MenuList';
 import { Player } from './components/Player';
 import { Search } from "./components/Search";
 import { Filter } from "./components/Filter";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -592,35 +593,7 @@ function App() {
               </div>
             </div>
             <div className="sidebar__block">
-              <div className="sidebar__list">
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-              </div>
+              <Sidebar />
             </div>
           </div>
         </main>
