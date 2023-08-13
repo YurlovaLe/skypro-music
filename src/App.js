9f98f7083db540149a9d6674bb4f7502f432a457
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-08-13 22:21:58 +0700

    add Search and Filter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { MenuList } from './components/MenuList';
 import { Player } from './components/Player';
+import { Search } from "./components/Search";
+import { Filter } from "./components/Filter";
 
 function App() {
   return (
@@ -21,28 +23,9 @@ function App() {
             </div>
           </nav>
           <div className="main__centerblock centerblock">
-            <div className="centerblock__search search">
-              <svg className="search__svg">
-                <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-              </svg>
-              <input
-                className="search__text"
-                type="search"
-                placeholder="Поиск"
-                name="search"
-              />
-            </div>
+            <Search />
             <h2 className="centerblock__h2">Треки</h2>
-            <div className="centerblock__filter filter">
-              <div className="filter__title">Искать по:</div>
-              <div className="filter__button button-author _btn-text">
-                исполнителю
-              </div>
-              <div className="filter__button button-year _btn-text">
-                году выпуска
-              </div>
-              <div className="filter__button button-genre _btn-text">жанру</div>
-            </div>
+            <Filter />
             <div className="centerblock__content">
               <div className="content__title playlist-title">
                 <div className="playlist-title__col col01">Трек</div>
