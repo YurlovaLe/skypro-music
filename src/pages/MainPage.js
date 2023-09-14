efaa50cc93d23f7a1387df66fbb9b9cf9ff66a19
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-09-14 14:25:24 +0700

    get playlist from api

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,7 +3,8 @@ import { MenuList } from "../components/MenuList/MenuList";
 import { Player } from "../components/Player/Player";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import { Tracklist } from "../components/Tracklist/Tracklist";
-import { tracks } from "../mocks/tracks";
+import { getAllTracks } from "../api";
+// import { tracks } from "../mocks/tracks";
 import * as S from "../App.style";
 
 function MainPage() {
@@ -19,13 +20,22 @@ function MainPage() {
     };
   }, []);
 
+  const [alltracks, setAlltracks] = useState();
+
+  useEffect(() => {
+    getAllTracks().then((playlist) => {
+      console.log(playlist)
+      setAlltracks(playlist)
+    });
+  }, []);
+
   return (
     <>
       <S.Wrapper>
         <S.Container>
           <S.Main>
             <MenuList />
-            <Tracklist isLoading={isLoading} items={tracks} />
+            <Tracklist isLoading={isLoading} items={alltracks} />
             <S.MainSidebar>
               <S.SidebarPersonal>
                 <S.SidebarPersonalName>Sergey.Ivanov</S.SidebarPersonalName>
