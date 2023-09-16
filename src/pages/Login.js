49c010d4c474dd8fb14c16ac519ec506b9f95450
Lesia Yurlova <lesia.yurlova+1@yandex.ru>
2023-09-16 16:53:08 +0700

    hw6

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,12 @@
 export const Login = () => {
+  const handleClick = () => {
+    window.localStorage.setItem('user', '123456');
+    window.location.href = '/';
+  }
+
   return (
     <div>
-      <h1>Login</h1>
+      <button onClick={handleClick}>Login</button>
     </div>
   );
 }
